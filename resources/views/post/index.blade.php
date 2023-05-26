440d850094a09dc737d14c2822d8ebb475f90657
TAKATSU MARINA <75340313+marina-t1123@users.noreply.github.com>
2023-05-27 00:02:28 +0900

    一旦プッシュ。

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -4,12 +4,12 @@
     <!-- メイン -->
     <main>
         <div class="col-8 mx-auto text-center my-5">
-            <div class="h1">投稿一覧</div>
-
+            <h1>投稿一覧</h1>
+            <a href="{{ route('post.create')}}" class="btn btn-primary">投稿新規作成</a>
         </div>
 
-
         <!-- 一覧 -->
+
 
 
     </main>
--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,27 +1,6 @@
 @extends('layouts.parent')
 
 @section('content')
-    {{-- <header class="" style="height: 50px">
-        <nav class="navbar navbar-expand-lg navbar-light">
-          <div class="container">
-            <a class="navbar-brand text-lg" href="#">Lobook</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div>
-              <ul class="navbar-nav">
-                <li class="nav-item">
-                  <a class="nav-link" href="{{ route('register') }}">Sing up</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="{{ route('login') }}">Log in</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </header> --}}
-    <!-- カルーセル -->
     <div id="carouselExampleControls" class="carousel slide w-75 mx-auto my-5" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
@@ -30,12 +9,6 @@
             <div class="carousel-item">
                 <img src="{{ asset('images/top2.png') }}" class="d-block w-100 h-100" alt="...">
             </div>
-            {{-- <div class="carousel-item active">
-                <img src="/images/top1.png" class="d-block w-100 h-75" alt="...">
-            </div>
-            <div class="carousel-item">
-                <img src="/images/top2.png" class="d-block w-100 h-100" alt="...">
-            </div> --}}
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
